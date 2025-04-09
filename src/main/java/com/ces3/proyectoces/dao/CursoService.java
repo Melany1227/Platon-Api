87f430499a2f3b82d87c4579bf301627184401d1
Melany <melanyjsuarez@gmail.com>
2025-04-09 18:34:34 -0500

    Test

--- src/main/java/com/ces3/proyectoces/dao/CursoService.java
+++ src/main/java/com/ces3/proyectoces/dao/CursoService.java
@@ -7,8 +7,14 @@ import java.util.List;
 
 public class CursoService {
     private List<Curso> cursos = new ArrayList<>();
-    
+
     public List<Curso> listarCursos() {
         return cursos;
     }
+
+    public void agregarCurso(Curso nuevoCurso) {
+        
+    }
+
+
 }
